f2e4ff4614410c9eb22e2415750b29c118c12264
unknown <sizov.k@WS-6K-102-A02.ccam.cmc.unn.net>
2018-09-12 14:36:52 +0300

    fix bags

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 //возвращает новое рандомное число от 0 до 1
 double new_rand()
 {
-	return rand() / RAND_MAX;
+	return ((double)rand()) / RAND_MAX;
 }
 
 //генерирует рандомный массив чисел
 void generated_rand_mas(double * mas, size_t size) 
 {
-	for (size_t i = 0; i <= size; i++)
+	for (size_t i = 0; i < size; i++)
 		mas[i] = new_rand();
 }
 
 //выводит на экран массив чисел
-void print(float * mas, size_t size)
+void print(double * mas, size_t size)
 {
 	for (size_t  i = 0; i < size; i++)
 	{
@@ -26,10 +27,28 @@ void print(float * mas, size_t size)
 
 //смена элементов
 void swap(double &a, double &b)
-{
-	tmp = b;
+{	
+	double tmp;
+	tmp = a;
 	a = b;
 	b = tmp;
+}
+
+//сортировка выбором
+void selection_sort(double * mas, size_t  size) {
+	for (size_t idx_i = 0; idx_i < size - 1; idx_i++) {
+		size_t min_idx = idx_i;
+		for (size_t idx_j = idx_i + 1; idx_j < size; idx_j++)
+		{
+			if (mas[idx_j] < mas[min_idx])
+			    min_idx = idx_j;
+		}
+
+		if (min_idx != idx_i) {
+			swap(mas[idx_i], mas[min_idx]);
+			min_idx = idx_i;
+		}
+	}
 }
 
 //main
@@ -40,11 +59,8 @@ int main(int argc, char * argv[])
 		return 1;
 	}
 
-	const int size;
-	double mas[size];
-	int seed = atoi(argv[1]);
-
-	srand(seed);
+	const int size = atoi(argv[1]);
+	double* mas = new double[size];
 
 	generated_rand_mas(mas, size);
 	
@@ -54,22 +70,7 @@ int main(int argc, char * argv[])
 
 	print(mas, size);
 
+	delete[] mas;
+
 	return 0;
-}
-
-//сортировка выбором
-void selection_sort(double * mas, size_t  size) {
-	for (size_t idx_i = 0; idx_i < size - 1; idx_i++) {
-		int min_idx = idx_i;
-		for (size_t idx_j = idx_i + 1; idx_j < size; idx_j++)
-		{
-			if (mas[idx_j] < mas[min_idx]);
-			    min_idx = idx_j;
-		}
-
-		if (min_idx = idx_i) {
-			swap(mas[idx_i], mas[min_idx]);
-			min_idx = idx_i;
-		}
-	}
 }
